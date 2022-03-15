8fe5922ae9412d2347c3df41c19cbb0edd7be21c
nelson.kurniawan <nelson.kurniawan@gdn-commerce.com>
2022-03-15 14:15:20 +0700

    pagination

--- src/main/java/com/demo/spring/data/web/controller/ProductController.java
+++ src/main/java/com/demo/spring/data/web/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.demo.spring.data.entity.Shop;
 import com.demo.spring.data.service.ProductService;
 import com.demo.spring.data.validation.ProductExist;
 import com.demo.spring.data.web.model.request.product.CreateProductRequest;
+import com.demo.spring.data.web.model.request.product.FilterProductRequest;
 import com.demo.spring.data.web.model.request.product.UpdateProductRequest;
 import com.demo.spring.data.web.model.response.Response;
 import com.demo.spring.data.web.model.response.product.ProductResponse;
@@ -13,12 +14,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Api
 @Validated
@@ -51,6 +54,21 @@ public class ProductController {
                 .status(HttpStatus.OK.value())
                 .data(toResponse(product))
                 .build();
+    }
+
+    @ApiOperation("Filter Product")
+    @PostMapping(
+        path = "/api/products/filter",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response<List<ProductResponse>> filter(@RequestBody FilterProductRequest request) {
+        Page<Product> productPage = productService.findByFilter(request);
+        List<ProductResponse> productResponseList = productPage.map(this::toResponse).getContent();
+        return Response.<List<ProductResponse>>builder()
+            .status(HttpStatus.OK.value())
+            .data(productResponseList)
+            .pagination(toResponse(productPage))
+            .build();
     }
 
     @ApiOperation("update product")
@@ -89,5 +107,13 @@ public class ProductController {
         ShopResponse shopResponse = ShopResponse.builder().build();
         BeanUtils.copyProperties(shop, shopResponse);
         return shopResponse;
+    }
+
+    private Response.Pagination toResponse(Page<?> page) {
+        return Response.Pagination.builder()
+            .page(page.getNumber())
+            .size((long) page.getSize())
+            .totalItems(page.getTotalElements())
+            .build();
     }
 }
